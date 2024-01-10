079e8ddc2b6ee8c329e1d07648f05e2356801138
Thiago Batista <cavalcantibatista@gmail.com>
2024-01-10 14:28:33 +0000

    List all, path, Update password setted on service and controller.

--- demo-park-api/src/main/java/com/parking/demoparkapi/web/controller/UsuarioController.java
+++ demo-park-api/src/main/java/com/parking/demoparkapi/web/controller/UsuarioController.java
@@ -5,10 +5,9 @@ import com.parking.demoparkapi.service.UsuarioService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -22,5 +21,23 @@ public class UsuarioController {
         Usuario user = usuarioService.salvar(usuario);
         return ResponseEntity.status(HttpStatus.CREATED).body(user);
     }
+
+     @GetMapping("/{id}")
+    public ResponseEntity<Usuario> getById(@PathVariable Long id) {
+        Usuario user = usuarioService.buscarPorId(id);
+        return ResponseEntity.ok(user);
+    }
+    @PatchMapping("/{id}")
+    public ResponseEntity<Usuario> updatePassword(@PathVariable Long id, @RequestBody Usuario usuario) {
+        Usuario user = usuarioService.editarSenha(id, usuario.getPassword());
+        return ResponseEntity.ok(user);
+    }
+
+    @GetMapping
+    public  ResponseEntity<List<Usuario>> getAll() {
+        List<Usuario> users = usuarioService.buscarTodos();
+        return ResponseEntity.ok(users);
+    }
+
 
 }
