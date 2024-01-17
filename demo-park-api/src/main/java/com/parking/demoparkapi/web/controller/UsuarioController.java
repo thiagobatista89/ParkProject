a341d1da90b834fdcfbe74f30af7263d9e3d3a38
Thiago Batista <cavalcantibatista@gmail.com>
2024-01-17 16:31:32 +0000

    Added first unit test

--- demo-park-api/src/main/java/com/parking/demoparkapi/web/controller/UsuarioController.java
+++ demo-park-api/src/main/java/com/parking/demoparkapi/web/controller/UsuarioController.java
@@ -6,6 +6,12 @@ import com.parking.demoparkapi.web.dto.UsuarioCreateDto;
 import com.parking.demoparkapi.web.dto.UsuarioResponseDto;
 import com.parking.demoparkapi.web.dto.UsuarioSenhaDto;
 import com.parking.demoparkapi.web.dto.mapper.UsuarioMapper;
+import com.parking.demoparkapi.web.exception.ErrorMessage;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -13,31 +19,42 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@Tag(name = "usuários", description = "Cotém todas as operações.")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("api/v1/usuarios")
 public class UsuarioController {
 
     private final UsuarioService usuarioService;
-
+    @Operation(summary = "Criar um novo usuário", description = "Recurso para criar um novo usuário", responses = {
+            @ApiResponse(responseCode = "201", description = "Recurso criado com sucesso.", content = @Content(mediaType = "aplication/json", schema = @Schema(implementation = UsuarioResponseDto.class))),
+            @ApiResponse(responseCode = "409", description = "Username já cadastrado no sistema", content = @Content(mediaType = "aplication/json", schema = @Schema(implementation = ErrorMessage.class))),
+            @ApiResponse(responseCode = "422", description = "Recurso não processado por dados de entrada inválidos", content = @Content(mediaType = "aplication/json", schema = @Schema(implementation = ErrorMessage.class)))
+    })
     @PostMapping
     public ResponseEntity<UsuarioResponseDto> create(@Valid @RequestBody UsuarioCreateDto createDto) {
         Usuario user = usuarioService.salvar(UsuarioMapper.toUsuario((createDto)));
         return ResponseEntity.status(HttpStatus.CREATED).body(UsuarioMapper.toDto(user));
     }
-
      @GetMapping("/{id}")
     public ResponseEntity<UsuarioResponseDto> getById(@PathVariable Long id) {
         Usuario user = usuarioService.buscarPorId(id);
         return ResponseEntity.ok(UsuarioMapper.toDto(user));
     }
+    @Operation(summary = "Atualizar senha", description = "Atualizar senha.", responses = {
+            @ApiResponse(responseCode = "204", description = "Senha criada com sucesso.", content = @Content(mediaType = "aplication/json", schema = @Schema(implementation = void.class))),
+            @ApiResponse(responseCode = "400", description = "Senha não confere.", content = @Content(mediaType = "aplication/json", schema = @Schema(implementation = ErrorMessage.class))),
+            @ApiResponse(responseCode = "404", description = "Recurso não encontrado.", content = @Content(mediaType = "aplication/json", schema = @Schema(implementation = ErrorMessage.class)))
+    })
     @PatchMapping("/{id}")
     public ResponseEntity<Void> updatePassword(@PathVariable Long id,@Valid @RequestBody UsuarioSenhaDto dto) {
         Usuario user = usuarioService.editarSenha(id, dto.getSenhaAtual(), dto.getNovaSenha(), dto.getConfirmaSenha());
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(summary = "Listar todos os usuários", description = "Recurso para Listar todos os usuários", responses = {
+            @ApiResponse(responseCode = "200", description = "Lista de todos os usuários.", content = @Content(mediaType = "aplication/json", schema = @Schema(implementation = UsuarioResponseDto.class))),
+    })
     @GetMapping
     public  ResponseEntity<List<UsuarioResponseDto>> getAll() {
         List<Usuario> users = usuarioService.buscarTodos();
---
+++ demo-park-api/src/test/resources/sql/usuarios/usuarios-insert.sql
@@ -0,0 +1,1 @@
+INSERT INTO USUARIOS (id, name, password, role) values(100, 'testsql1@gmail.com', '123456', 'ROLE_ADMIN');
